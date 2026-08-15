ee8a80c16f7f50d3ed185f00121632cfab1589c8
Kevin <kevin12garciaavendano@gmail.com>
2026-08-15 17:20:49 -0500

    jugador

--- Clases/Jugador.java
+++ Clases/Jugador.java
@@ -10,9 +10,9 @@ public class Jugador {
       this.dorsal = dorsal;
       this.posicion = posicion;
       this.equipo = equipo;
-    };
+    }
     public void mostrarInfo(){
-        System.out.println("EL nombre del jugador es: " + nombre);
-    };
+        System.out.println("Los datos del jugador son: " + nombre + ", " + dorsal + ", " + posicion + ", " + equipo);
+    }
 
 }
--- Clases/Main.java
+++ Clases/Main.java
@@ -1,6 +1,7 @@
 package grupo2_LigaFutbol.Clases;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Jugador jugador1 = new Jugador("kevin", 14, "mediocampista", "real madrid");
-    };
-};
+        jugador1.mostrarInfo();
+    }
+}
